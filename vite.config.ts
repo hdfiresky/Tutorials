8602250e6d9cbac8cb40d2ffe994f225caf8bbec
hdfiresky <hdfiresky@gmail.com>
2025-08-17 17:22:01 +0530

    feat: deploy init: :

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,58 @@
-import path from 'path';
-import { defineConfig, loadEnv } from 'vite';
+import path from "path";
+import { VitePWA } from 'vite-plugin-pwa'; 
+import { defineConfig, loadEnv } from "vite";
 
 export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
-    return {
-      define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
+  const env = loadEnv(mode, ".", "");
+  return {
+    base: "/tutorial/",
+    define: {
+      "process.env.API_KEY": JSON.stringify(env.GEMINI_API_KEY),
+      "process.env.GEMINI_API_KEY": JSON.stringify(env.GEMINI_API_KEY),
+    },
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "."),
       },
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
-      }
-    };
+    },
+    plugins: [
+      VitePWA({
+        registerType: "autoUpdate",
+        includeAssets: [
+          "robots.txt",
+          "favicon.png",
+          "apple-touch-icon.webp",
+          "pwa-192x192.webp",
+          "pwa-512x512.webp",
+        ],
+        manifest: {
+          name: "Tutorial",
+          short_name: "Tutorial",
+          start_url: "/tutorial/",
+          scope: "/tutorial/",
+          display: "standalone",
+          background_color: "#ffffff",
+          theme_color: "#ffffff",
+          icons: [
+            {
+              src: "/tutorial/pwa-192x192.png",
+              sizes: "192x192",
+              type: "image/webp",
+            },
+            {
+              src: "/tutorial/pwa-512x512.png",
+              sizes: "512x512",
+              type: "image/webp",
+            },
+            {
+              src: "/tutorial/pwa-512x512.png",
+              sizes: "512x512",
+              type: "image/webp",
+              purpose: "any maskable",
+            },
+          ],
+        },
+      }),
+    ],
+  };
 });
